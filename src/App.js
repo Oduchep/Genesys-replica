3296568e8ff55b2388aff49d8a136421b0a5a976
Oduchep <oduchep@gmail.com>
2020-10-13 11:49:13 +0100

    still struggling with the landing page, power supply has been horrible

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import Genesys from "./Genesys";
+
+function App() {
+  return <Genesys />;
+}
+
+export default App;
---
+++ src/Genesys.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LandingPage from "./LandingPage";
+
+function Genesys() {
+  return <LandingPage />;
+}
+
+export default Genesys;
